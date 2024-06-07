55971b7d16a3c0eaa12df666ba7285f7749654d6
max <derstadlmax@gmail.com>
2024-06-07 11:00:35 +0200

    changed style of setDefaultLocation Btn

--- src/helpers/weatherify/markup/generateButtonMarkup.ts
+++ src/helpers/weatherify/markup/generateButtonMarkup.ts
@@ -2,6 +2,7 @@ export default function generateButton(textContent: string, id: string) {
   const button = document.createElement('button');
   button.textContent = textContent;
   button.id = id;
+  button.classList.add('btn', 'btn-sm');
 
   return button;
 }
