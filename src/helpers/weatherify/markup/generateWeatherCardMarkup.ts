a52120e624838c8b0b03cdab7d7844ac67d9b4e0
max <derstadlmax@gmail.com>
2024-06-10 15:47:11 +0200

    changed layout and implemented that only two location words are shown instead of the full location name

--- src/helpers/weatherify/markup/generateWeatherCardMarkup.ts
+++ src/helpers/weatherify/markup/generateWeatherCardMarkup.ts
@@ -5,36 +5,42 @@ const weatherContainer =
 
 const weatherCard = document.createElement('section');
 weatherCard.id = 'weather-card';
+weatherCard.classList.add('bg-blue-600', 'rounded-lg', 'p-2', 'pt-4');
 export default function generateWeatherCard(weather: CurrentWeatherData) {
+  // Create a div element to hold the weather info
+  const infoContainer = document.createElement('div');
+  infoContainer.classList.add('flex', 'items-center');
+
+  // Create a paragraph element to display the location
+  const infoDescription = document.createElement('p');
+  infoDescription.textContent = `${weather.description}`;
+
   // Create an image element
   const img = document.createElement('img');
   img.src = `https://openweathermap.org/img/wn/${weather.icon}@2x.png`;
   img.alt = 'Weather Icon';
 
-  // Create a div element to hold the weather info
-  const infoContainer = document.createElement('div');
-
-  // Create a paragraph element to display the location
-  const infoDescription = document.createElement('p');
-  // infoDescription.classList.add('text-3xl');
-  // infoDescription.classList.add('underline');
-  infoDescription.textContent = `${weather.description}`;
-
   // Create a paragraph element to display the temperature
   const infoTemp = document.createElement('p');
   infoTemp.textContent = `${Math.trunc(weather.temp)}°C`;
+  infoTemp.classList.add('text-5xl', 'font-semibold');
 
   // Create a paragraph element to display the location name
   const infoLocationName = document.createElement('p');
-  infoLocationName.textContent = `${weather.name}`;
+  const nameParts = weather.name.split(',');
+  let displayName =
+    nameParts.length > 2 ? `${nameParts[0]}, ${nameParts[1]}` : weather.name;
+  infoLocationName.textContent = displayName;
+  infoLocationName.classList.add('text-xl', 'font-semibold');
 
   // Append the elements to the weather card
   weatherCard.innerHTML = '';
+
+  weatherCard.appendChild(infoLocationName);
+  weatherCard.appendChild(infoDescription);
   weatherCard.appendChild(infoContainer);
   infoContainer.appendChild(img);
-  infoContainer.appendChild(infoDescription);
   infoContainer.appendChild(infoTemp);
-  infoContainer.appendChild(infoLocationName);
 
   if (weatherContainer && !weatherContainer.contains(weatherCard)) {
     weatherContainer.appendChild(weatherCard);
